6f836e3fe9b937605fc1633f42126c2161ab0999
B-Audrey <bonis.audreyy@gmail.com>
2023-02-07 17:57:09 +0100

    Boutons filtres fonctionnels

--- FrontEnd/javaScript/gallery.js
+++ FrontEnd/javaScript/gallery.js
@@ -1,65 +1,36 @@
-//Generate display with Works results
+/////////////////////////VARIABLES///////////////////////
 
 
-//link to the works route
-async function getWorks(){
-    //call works route
-    const response = await fetch('http://localhost:5678/api/works');
-    //convert response to json
-    return response.json();  
-}
 
-//fonction to display
+
+
+//////////////////////////FUNCTIONS//////////////////////
 function display(works){
+    
     for(let i=0; i< works.length; i++) {
 
-        //point the gallery tag
         const gallery = document.querySelector('.gallery');
-
-        //create the element tag for each work
         const worksElement = document.createElement('figure');
-
-        //fill with the appropriate img and text
+        
         const imgPart = document.createElement('img');
         imgPart.src = works[i].imageUrl;
         imgPart.crossOrigin = "anonymous"
         imgPart.alt = works[i].title;
-        //link with worksElement
         worksElement.appendChild(imgPart);
-
+       
         const txtPart = document.createElement('figcaption');
         txtPart.innerText = works[i].title;
-        //link with worksElement
         worksElement.appendChild(txtPart);
 
         gallery.appendChild(worksElement);
     }
  }
 
- // function, waiting the response from getWorks before starting
- async function displayWorks(){
-     const works = await getWorks();
-     console.log(works);
-     display(works);
- }
-
-// call function to display
-displayWorks();
-
-
-// Generate butons to filter
-async function getCategories(){
-    //call works route
-    const response = await fetch('http://localhost:5678/api/categories');
-    //convert response to json
-    return response.json();  
-}
-
 function formatClassName (string){
-    return string.toLowerCase().replaceAll(' ','_');
+    return string.toLowerCase().replaceAll(' ','_').replace('&','');
 }
 
-function displayButton (categories){
+function createButton (categories){
     const filtres = document.querySelector('.filtres');
     filtres.innerHTML= `<button class="tous" type="button">Tous</button>`;
     for (let i=0; i<categories.length; i++){
@@ -67,12 +38,74 @@ function displayButton (categories){
     }
 }
 
-async function getFilterButton(){
+async function displayWorks(){
+    const works = await getWorks();
+    console.log(works);
+    display(works);
+}
+
+async function displayFilterButton(){
     const categories = await getCategories()
     console.log(categories);
-    displayButton(categories);
+    createButton(categories);
 }
 
 
+/////////////////////////FUNCTION LINK WITH API////////////////////
+async function getWorks(){
+    const response = await fetch('http://localhost:5678/api/works');
+    //convert response to json
+    return response.json(); 
+}
 
-getFilterButton();
+async function getCategories(){
+    //call works route
+    const response = await fetch('http://localhost:5678/api/categories');
+    //convert response to json
+    return response.json();  
+}
+///////////////////////////////////CALLS AND DISPLAYS////////////////////////
+
+async function activeFilter(){
+    const works = await getWorks();
+    await displayWorks();
+    await displayFilterButton();
+    const tousBtn = document.querySelector('.tous');
+    const objetsBtn = document.querySelector('.objets');
+    const appartementsBtn = document.querySelector('.appartements');
+    const hotelsRestaurantsBtn = document.querySelector('.hotels__restaurants');
+
+    tousBtn.addEventListener('click', function(){
+        document.querySelector('.gallery').innerHTML=('');
+        display(works);
+    });
+
+    objetsBtn.addEventListener('click', function(){
+        const objets = works.filter(function(work) {
+            return work.categoryId === 1;
+        })
+        console.log(objets);
+        document.querySelector('.gallery').innerHTML=('');
+        display(objets);
+    });
+    
+    appartementsBtn.addEventListener('click', function(){
+        const appartements = works.filter(function(work){
+            return work.categoryId ===2;
+        })
+        console.log(appartements);
+        document.querySelector('.gallery').innerHTML=('');
+        display(appartements);
+    });
+
+    hotelsRestaurantsBtn.addEventListener('click', function(){
+        const hotelsRestaurants = works.filter(function(work){
+            return work.categoryId === 3;
+        })
+        console.log(hotelsRestaurants);
+        document.querySelector('.gallery').innerHTML=('');
+        display(hotelsRestaurants);
+    });
+}
+
+activeFilter();
