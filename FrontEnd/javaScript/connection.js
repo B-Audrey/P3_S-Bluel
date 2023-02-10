7af9823d337bd67f35e925ddd58a11307c178f20
B-Audrey <bonis.audreyy@gmail.com>
2023-02-10 14:03:58 +0100

    Liens et logIn de connexion ok

--- FrontEnd/javaScript/connection.js
+++ FrontEnd/javaScript/connection.js
@@ -1,20 +1,35 @@
 const sendingButton = document.querySelector("#form");
-
-sendingButton.addEventListener('submit', function(event){
+sendingButton.addEventListener('submit', async function(event){
+    let dataToSend = {email:'', password:''};
+    console.log(dataToSend.email);
     event.preventDefault();
-    console.log(event);
     const logInData = {
         email: event.target.querySelector('#email').value,            
         password: event.target.querySelector('#password').value
     }
-    const dataToSend = JSON.stringify(logInData);
-    logIn(dataToSend);
-});
+    dataToSend = JSON.stringify(logInData);
+    console.log("infos en json >>>>>> " + dataToSend.email);
 
-async function logIn(dataToSend){
-    await fetch("http://localhost:5678/api/users/login", {
-    method:"POST",
-    headers : {"content-type": "application/json"},
-    body: dataToSend
-    });
-}
+    if (dataToSend.email !== undefined && dataToSend.password !== undefined){
+        console.log('je rentre dans le if');
+        let response = await fetch("http://localhost:5678/api/users/login", {
+            method:"POST",
+            headers : {"content-type": "application/json"},
+            body: dataToSend
+        });
+        response = await response.json();
+        await console.log(response);
+        let token = '';
+            if(response.message == 'user not found'){
+                alert('Utilisateur ou Mot de passe incorrect. Merci de vérifier les données saisies')
+            } else if(response.token !== ''){
+                token = response.token;
+                document.location.href = 'http://localhost:5500/FrontEnd/index.html';
+            } else {
+                alert('Données non valides')
+            }
+    } else {
+        console.log('je rentre dans le else');
+        alert('Merci de remplir les champs de saisie');
+    }
+});
