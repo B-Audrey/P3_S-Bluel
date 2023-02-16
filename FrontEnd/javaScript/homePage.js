822805ef1061b575cedd88871c53eda9b6b1cb56
B-Audrey <bonis.audreyy@gmail.com>
2023-02-16 10:33:03 +0100

    Création Modale

--- FrontEnd/javaScript/homePage.js
+++ FrontEnd/javaScript/homePage.js
@@ -4,4 +4,18 @@ const works = useSet(jsonWorks);
 const jsonCategories = await getCategories();
 const categories = useSet(jsonCategories);
 
+
+const openModalButton = document.getElementById('openModalButton'); // selectionne le bouton modal du DOM
+const closeModalButton = document.getElementById('closeModalButton');
+const modalBlock = document.getElementById('modalBlock'); // selectionne la div du bloc : #modal
+
+openModalButton.addEventListener('click', () => { 
+    modalBlock.style.display = 'flex'; //
+});
+
+closeModalButton.addEventListener('click', () => {
+    modalBlock.style.display = 'none';
+});
+
+
 display(works);
